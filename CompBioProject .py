eab99d9ea8066e9443149e6830102f54c81aab10
MaddieTM <maddiectm@gmail.com>
2014-07-24 16:08:56 -0400

    Update CompBioProject .py

--- CompBioProject .py
+++ CompBioProject .py
@@ -6,7 +6,9 @@ import sys
 import sys.argv
 
 def plotOutput(times, currentMolecules, numberOfMolecules):
-//Creates and displays a graph in a new window of concentration versus time with lines of data points for each molecule
+""
+Creates and displays a graph in a new window of concentration versus time with lines of data points for each molecule
+"
     moleculeData = []
     for molecule in numberOfMolecules:
         moleculeData = []
@@ -36,7 +38,11 @@ Finds the propensity of each reaction occurring, returns a list of the propensit
             i = i+1
     return props
                             
-def reactUpdater(numberOfMolecules, reaction): # Carries out given reaction
+def reactUpdater(numberOfMolecules, reaction): 
+""
+Subtracts the specified number of reactants and adds the products to the molecule amounts to represent a reaction 
+being carried out. 
+""
     for sideOfReaction in reaction:
             for molecule in sideOfReaction:
                 numberOfMolecules[molecule] += sideOfReaction[molecule]
